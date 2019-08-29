312dc9dd0ddbb351c4b9ec60395871c7f6dfbe62
KuaiYu95 <kuaiyumobile@gmail.com>
2019-08-29 11:35:49 +0800

    update build

---
+++ build/precache-manifest.c2924bd421a65112b3f2206afb44bd70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce27de73c75697dcabaa43e2a0fdd4c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9d19f41687d964f3fac",
+    "url": "./static/css/main.dfb6742c.chunk.css"
+  },
+  {
+    "revision": "0e40b4d6d99e416049da",
+    "url": "./static/js/2.bf3d8e00.chunk.js"
+  },
+  {
+    "revision": "a9d19f41687d964f3fac",
+    "url": "./static/js/main.d0c2f127.chunk.js"
+  },
+  {
+    "revision": "617f4de50f6d12b986bf",
+    "url": "./static/js/runtime~main.1d62a3c5.js"
+  }
+]);
